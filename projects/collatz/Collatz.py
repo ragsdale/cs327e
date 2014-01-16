c9ed237d13b416dcc35dbd7171e34886591d8abd
Glenn Downing <downing@cs.utexas.edu>
2014-01-16 15:42:21 -0600

    another commit

--- projects/collatz/Collatz.py
+++ projects/collatz/Collatz.py
@@ -10,28 +10,35 @@
 # collatz_read
 # ------------
 
-def collatz_read (r) :
+def collatz_read (r, a) :
     """
+    reads two ints into a[0] and a[1]
     r is a  reader
-    returns an generator that iterates over a sequence of lists of ints of length 2
-    for s in r :
-        l = s.split()
-        b = int(l[0])
-        e = int(l[1])
-        yield [b, e]
+    a is an array of int
+    return true if that succeeds, false otherwise
     """
-    return (map(int, s.split()) for s in r)
+    s = r.readline()
+    if s == "" :
+        return False
+    l = s.split()
+    a[0] = int(l[0])
+    a[1] = int(l[1])
+    assert a[0] > 0
+    assert a[1] > 0
+    return True
 
 # ------------
 # collatz_eval
 # ------------
 
-def collatz_eval ((i, j)) :
+def collatz_eval (i, j) :
     """
     i is the beginning of the range, inclusive
     j is the end       of the range, inclusive
     return the max cycle length in the range [i, j]
     """
+    assert i > 0
+    assert j > 0
     # <your code>
     v = 1
     assert v > 0
@@ -41,13 +48,13 @@ def collatz_eval ((i, j)) :
 # collatz_print
 # -------------
 
-def collatz_print (w, (i, j), v) :
+def collatz_print (w, i, j, v) :
     """
     prints the values of i, j, and v
     w is a writer
-    v is the max cycle length
     i is the beginning of the range, inclusive
     j is the end       of the range, inclusive
+    v is the max cycle length
     """
     w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
 
@@ -61,6 +68,7 @@ def collatz_solve (r, w) :
     r is a reader
     w is a writer
     """
-    for t in collatz_read(r) :
-        v = collatz_eval(t)
-        collatz_print(w, t, v)
+    a = [0, 0]
+    while collatz_read(r, a) :
+        v = collatz_eval(a[0], a[1])
+        collatz_print(w, a[0], a[1], v)
--- projects/collatz/Collatz2.py
+++ projects/collatz/Collatz2.py
@@ -10,35 +10,28 @@
 # collatz_read
 # ------------
 
-def collatz_read (r, a) :
+def collatz_read (r) :
     """
-    reads two ints into a[0] and a[1]
     r is a  reader
-    a is an array of int
-    return true if that succeeds, false otherwise
+    returns an generator that iterates over a sequence of lists of ints of length 2
+    for s in r :
+        l = s.split()
+        b = int(l[0])
+        e = int(l[1])
+        yield [b, e]
     """
-    s = r.readline()
-    if s == "" :
-        return False
-    l = s.split()
-    a[0] = int(l[0])
-    a[1] = int(l[1])
-    assert a[0] > 0
-    assert a[1] > 0
-    return True
+    return (map(int, s.split()) for s in r)
 
 # ------------
 # collatz_eval
 # ------------
 
-def collatz_eval (i, j) :
+def collatz_eval ((i, j)) :
     """
     i is the beginning of the range, inclusive
     j is the end       of the range, inclusive
     return the max cycle length in the range [i, j]
     """
-    assert i > 0
-    assert j > 0
     # <your code>
     v = 1
     assert v > 0
@@ -48,13 +41,13 @@ def collatz_eval (i, j) :
 # collatz_print
 # -------------
 
-def collatz_print (w, i, j, v) :
+def collatz_print (w, (i, j), v) :
     """
     prints the values of i, j, and v
     w is a writer
+    v is the max cycle length
     i is the beginning of the range, inclusive
     j is the end       of the range, inclusive
-    v is the max cycle length
     """
     w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
 
@@ -68,7 +61,6 @@ def collatz_solve (r, w) :
     r is a reader
     w is a writer
     """
-    a = [0, 0]
-    while collatz_read(r, a) :
-        v = collatz_eval(a[0], a[1])
-        collatz_print(w, a[0], a[1], v)
+    for t in collatz_read(r) :
+        v = collatz_eval(t)
+        collatz_print(w, t, v)
