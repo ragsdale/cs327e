fd4a46e17f5a5d7605fedcb5aa5e0759acf5fbee
Glenn Downing <downing@cs.utexas.edu>
2014-01-29 16:53:06 -0600

    another commit

---
+++ quizzes/Quiz5.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+CS327e: Quiz #5 (5 pts) <Aizhuldyz>
+"""
+
+""" ----------------------------------------------------------------------
+ 1. Describe the difference between a unit test and an acceptance test.
+    (2 pts)
+
+a unit test tests the return value and the side effects of an individual
+function or method
+an acceptance test tests the input/output behavior of the whole program
+"""
+
+""" ----------------------------------------------------------------------
+ 2. Describe the expected behavior of the following. Be precise.
+    (1 pt)
+
+    % python3 RunCollatz.py
+
+nothing
+the program is silently waiting for input
+"""
+
+""" ----------------------------------------------------------------------
+ 3. Describe the expected behavior of the following. Be precise.
+    (1 pt)
+
+    % python3 TestCollatz.py
+
+outputs the results of Python's unittests
+"""
