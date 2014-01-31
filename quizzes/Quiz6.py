e51b072e6c5a88b0930587102ca930b28c334ede
Glenn Downing <downing@cs.utexas.edu>
2014-01-31 17:53:48 -0600

    another commit

---
+++ quizzes/Quiz6.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""
+CS327e: Quiz #6 (5 pts) <Fiona>
+"""
+
+""" ----------------------------------------------------------------------
+ 1. What are the SQL terms that correspond to the following Codd terms:
+    relation, attribute, tuple
+    [SQL: Ch.2, Pg. 34]
+    (3 pts)
+
+table, column, row
+"""
+
+""" ----------------------------------------------------------------------
+ 2. Tables have an attractive property called "closure". Explain it.
+    [SQL: Ch.2, Pg. 36]
+    (1 pt)
+
+any operation performed on a table yields another table
+"""
