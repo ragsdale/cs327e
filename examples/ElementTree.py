ee312ece6ce89d9745c2f30bd8cdb364ea5e669f
Glenn Downing <downing@cs.utexas.edu>
2014-02-04 22:03:42 -0600

    another commit

---
+++ examples/ElementTree.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+# --------------
+# ElementTree.py
+# --------------
+
+from xml.etree.ElementTree import Element, fromstring
+
+print("ElementTree.py")
+print()
+
+s = "<xml>" + "".join(open("ElementTree.xml")) + "</xml>"
+assert(type(s) is str)
+print(s)
+print()
+
+x = fromstring(s)
+assert(type(x) is Element)
+
+for v in x :
+    print(v)
+    for w in v :
+        print(w)
+    print()
+
+print("Done.")
+
+"""
+ElementTree.py
+
+<xml><THU>
+	<Team>
+		<ACRush></ACRush>
+		<Jelly></Jelly>
+		<Cooly></Cooly>
+	</Team>
+	<JiaJia>
+		<Team>
+			<Ahyangyi></Ahyangyi>
+			<Dragon></Dragon>
+			<Cooly><Amber></Amber></Cooly>
+		</Team>
+	</JiaJia>
+</THU>
+<Team><Cooly></Cooly></Team>
+</xml>
+
+<Element 'THU' at 0x10ba256d8>
+<Element 'Team' at 0x10ba25788>
+<Element 'JiaJia' at 0x10ba25940>
+
+<Element 'Team' at 0x10ba25c00>
+<Element 'Cooly' at 0x10ba25c58>
+
+Done.
+"""
