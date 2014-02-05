79a2bab510d2e5b90b28622ac08dc61d94970bfc
Glenn Downing <downing@cs.utexas.edu>
2014-02-05 17:11:00 -0600

    another commit

---
+++ quizzes/Quiz8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+CS327e: Quiz #8 (5 pts) <Fiona>
+"""
+
+""" ----------------------------------------------------------------------
+ 1. Which of the following practices demonstrates effective pair
+    programming?
+    [All I Really Needed to Know about Pair Programming I Learned in
+     Kindergarten]
+    (2 pts)
+
+a. Each partner writing separate parts.
+b. Each partner writing both parts and then submitting the best.
+c. Each partner writing both parts and then submitting the best
+   combination.
+d. Sharing a monitor and keyboard while coding.
+e. One partner writing the interface and tests, the other the
+   implementation.
+
+d.
+"""
+
+""" ----------------------------------------------------------------------
+ 2. Describe ping-pong paring.
+    [The Art of Agile Development]
+    (2 pts)
+
+In this exercise, one person writes a test. The other person makes it pass
+and writes a new test. Then the first person makes it pass and repeats the
+process by writing another test.
+"""
