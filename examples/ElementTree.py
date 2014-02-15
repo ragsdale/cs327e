63da47412c26da8c21762807b6d901dd421cc466
Glenn Downing <downing@cs.utexas.edu>
2014-02-15 11:38:13 -0600

    another commit

--- examples/ElementTree.py
+++ examples/ElementTree.py
@@ -9,11 +9,13 @@ from xml.etree.ElementTree import Element, fromstring, tostring
 print("ElementTree.py")
 print()
 
-def traverse (a, d = "") :
-    print(d + a.tag)
+def traverse (a, c = 0, d = "") :
+    print(d + "(" + str(c) + ") " + a.tag)
     for v in a :
-        traverse(v, d + "\t")
+        c += 1
+        c = traverse(v, c, d + "\t")
     print(d + "/" + a.tag)
+    return c
 
 s = "<xml>" + "".join(open("ElementTree.xml")) + "</xml>"
 assert(type(s) is str)
@@ -21,9 +23,16 @@ assert(type(s) is str)
 a = fromstring(s)
 assert(type(a) is Element)
 
+print("*** The Whole Tree ***")
 traverse(a)
 print()
 
+print("*** top two elements by indexing ***")
+print(a[0].tag)
+print(a[1].tag)
+print()
+
+print("*** top two elements by iterating ***")
 p = iter(a)
 
 e = next(p)
@@ -33,42 +42,77 @@ e = next(p)
 print(e.tag)
 print()
 
+print("*** child element Team ***")
+b = a.findall("Team")
+for v in b :
+    print(v)
+print()
+
+print("*** grandchild element JiaJia ***")
+b = a.findall("*/JiaJia")
+for v in b :
+    print(v)
+print()
+
+print("*** all elements Team ***")
+b = a.findall(".//Team")
+for v in b :
+    print(v)
+print()
+
 print("Done.")
 
 """
 ElementTree.py
 
-xml
-	THU
-		Team
-			ACRush
+*** The Whole Tree ***
+(0) xml
+	(1) THU
+		(2) Team
+			(3) ACRush
 			/ACRush
-			Jelly
+			(4) Jelly
 			/Jelly
-			Cooly
+			(5) Cooly
 			/Cooly
 		/Team
-		JiaJia
-			Team
-				Ahyangyi
+		(6) JiaJia
+			(7) Team
+				(8) Ahyangyi
 				/Ahyangyi
-				Dragon
+				(9) Dragon
 				/Dragon
-				Cooly
-					Amber
+				(10) Cooly
+					(11) Amber
 					/Amber
 				/Cooly
 			/Team
 		/JiaJia
 	/THU
-	Team
-		Cooly
+	(12) Team
+		(13) Cooly
 		/Cooly
 	/Team
 /xml
 
+*** top two elements by indexing ***
 THU
 Team
+
+*** top two elements by iterating ***
+THU
+Team
+
+*** child element Team ***
+<Element 'Team' at 0x10e0c8db8>
+
+*** grandchild element JiaJia ***
+<Element 'JiaJia' at 0x10e0c8af8>
+
+*** all elements Team ***
+<Element 'Team' at 0x10e0c8940>
+<Element 'Team' at 0x10e0c8b50>
+<Element 'Team' at 0x10e0c8db8>
 
 Done.
 """
