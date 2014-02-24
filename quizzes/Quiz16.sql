3d4259bd5bb6d62fa692af40eb053160b3afc7f3
Glenn Downing <downing@cs.utexas.edu>
2014-02-24 17:13:50 -0600

    another commit

---
+++ quizzes/Quiz16.sql
@@ -0,0 +1,20 @@
+/*
+CS327e: Quiz #16 (5 pts) <Fiona>
+*/
+
+/* -----------------------------------------------------------------------
+ 1. Write the SQL code to:
+    a. create a table R with columns A, B, C, with types int, float,
+       and text
+    b. show the tables
+    c. show the columns
+    (4 pts)
+*/
+
+create table R {
+    A int,
+    B float,
+    C text};
+
+show tables;
+show columns from R;
