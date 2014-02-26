85c6f83c4485d6704eebcf37455f7a04329db597
Glenn Downing <downing@cs.utexas.edu>
2014-02-26 16:50:03 -0600

    another commit

--- quizzes/Quiz17.sql
+++ quizzes/Quiz17.sql
@@ -15,19 +15,19 @@ create table College (cName text, state char(2), enrollment int);
 select sID, sName
     from Student
     inner join Apply
-    where (Student.sID = Apply.sID) and (cName == "Stanford") and (major = 'CS');
+    where (Student.sID = Apply.sID) and (cName = "Stanford") and (major = 'CS');
 
 select sID, sName
     from Student
     inner join Apply on Student.sID = Apply.sID
-    where (cName == "Stanford") and (major = 'CS');
+    where (cName = "Stanford") and (major = 'CS');
 
 select sID, sName
     from Student
     inner join Apply using (sID)
-    where (cName == "Stanford") and (major = 'CS');
+    where (cName = "Stanford") and (major = 'CS');
 
 select sID, sName
     from Student
     natural join Apply
-    where (cName == "Stanford") and (major = 'CS');
+    where (cName = "Stanford") and (major = 'CS');
