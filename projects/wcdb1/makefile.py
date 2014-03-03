c0af50eac0cb036939681d638e4c0e79bf9d9b2c
Glenn Downing <downing@cs.utexas.edu>
2014-03-03 13:26:51 -0600

    another commit

--- projects/wcdb1/makefile.py
+++ projects/wcdb1/makefile.py
@@ -1,5 +1,4 @@
 all:
-	make WCDB.zip
 
 clean:
 	rm -f *.pyc
@@ -7,40 +6,14 @@ clean:
 	rm -f RunWCDB.tmp
 	rm -f TestWCDB.out
 	rm -f WCDB.html
-	rm -f WCDB.log
-	rm -f WCDB.zip
 
 diff: RunWCDB.in RunWCDB.py WCDB.py
 	RunWCDB.py < RunWCDB.in > RunWCDB.tmp
 	diff RunWCDB.out RunWCDB.tmp
 	rm RunWCDB.tmp
 
-turnin-list:
-	turnin --list aizhuli cs327epj3
-
-turnin-submit: WCDB.zip
-	turnin --submit aizhuli cs327epj3 WCDB.zip
-
-turnin-verify:
-	turnin --verify aizhuli cs327epj3
-
 WCDB.html: WCDB.py
 	pydoc -w WCDB
-
-WCDB.log:
-	git log > WCDB.log
-
-WCDB.zip: makefile                          \
-          WCDB.html WCDB.log WCDB.py        \
-          WCDB.xml WCDB.xsd.xml             \
-          RunWCDB.in RunWCDB.out RunWCDB.py \
-          TestWCDB.py TestWCDB.out
-	zip -r WCDB.zip                          \
-           makefile                          \
-           WCDB.html WCDB.log WCDB.py        \
-           WCDB.xml WCDB.xsd.xml             \
-           RunWCDB.in RunWCDB.out RunWCDB.py \
-           TestWCDB.py TestWCDB.out
 
 RunWCDB.out: RunWCDB.in RunWCDB.py WCDB.py
 	RunWCDB.py < RunWCDB.in > RunWCDB.out
