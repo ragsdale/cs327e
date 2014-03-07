a3df172d73b17c34bb8546a41757d1f48f16704b
Glenn Downing <downing@cs.utexas.edu>
2014-03-07 14:38:27 -0600

    another commit

--- examples/Joins.py
+++ examples/Joins.py
@@ -4,58 +4,85 @@
 # Joins.py
 # --------
 
-def select (r, up) :
-    return [t for t in r if up(t)]
+def cross_join_1 (r, s) :
+    x = []
+    for v in r :
+        for w in s :
+            y = {}
+            for u in v :
+                y[u] = v[u]
+            for u in w :
+                y[u] = w[u]
+            x.append(y)
+    return x
 
-def project (r, *a) :
-    return [{v : t[v] for v in a if v in t} for t in r]
-
-def cross_join (r, s) :
+def cross_join_2 (r, s) :
     return [dict(list(v.items()) + list(w.items())) for v in r for w in s]
 
-def theta_join (r, s, bp) :
+def theta_join_1 (r, s, bp) :
+    x = []
+    for v in r :
+        for w in s :
+            if bp(v, w) :
+                y = {}
+                for u in v :
+                    y[u] = v[u]
+                for u in w :
+                    y[u] = w[u]
+                x.append(y)
+    return x
+
+def theta_join_2 (r, s, bp) :
     return [dict(list(v.items()) + list(w.items())) for v in r for w in s if bp(v, w)]
 
 print("Joins.py")
 
-R = [ \
+r = [ \
     {"A" : 1, "B" : 6},
     {"A" : 2, "B" : 7},
     {"A" : 3, "B" : 8}]
-assert(len(R) == 3)
+assert(len(r) == 3)
 
-S = [ \
+s = [ \
     {"A" : 4, "C" : 6},
     {"A" : 1, "C" : 7},
     {"A" : 2, "C" : 8},
     {"A" : 2, "C" : 9}]
-assert(len(S) == 4)
+assert(len(s) == 4)
 
-x = cross_join(R, S)
-assert(len(x) == 12)
-assert(
-    x
-    ==
-    [{'A': 4, 'B': 6, 'C': 6},
-     {'A': 1, 'B': 6, 'C': 7},
-     {'A': 2, 'B': 6, 'C': 8},
-     {'A': 2, 'B': 6, 'C': 9},
-     {'A': 4, 'B': 7, 'C': 6},
-     {'A': 1, 'B': 7, 'C': 7},
-     {'A': 2, 'B': 7, 'C': 8},
-     {'A': 2, 'B': 7, 'C': 9},
-     {'A': 4, 'B': 8, 'C': 6},
-     {'A': 1, 'B': 8, 'C': 7},
-     {'A': 2, 'B': 8, 'C': 8},
-     {'A': 2, 'B': 8, 'C': 9}])
+def test (f) :
+    x = f(r, s)
+    assert(len(x) == 12)
+    assert(
+        x
+        ==
+        [{'A': 4, 'B': 6, 'C': 6},
+         {'A': 1, 'B': 6, 'C': 7},
+         {'A': 2, 'B': 6, 'C': 8},
+         {'A': 2, 'B': 6, 'C': 9},
+         {'A': 4, 'B': 7, 'C': 6},
+         {'A': 1, 'B': 7, 'C': 7},
+         {'A': 2, 'B': 7, 'C': 8},
+         {'A': 2, 'B': 7, 'C': 9},
+         {'A': 4, 'B': 8, 'C': 6},
+         {'A': 1, 'B': 8, 'C': 7},
+         {'A': 2, 'B': 8, 'C': 8},
+         {'A': 2, 'B': 8, 'C': 9}])
 
-x = theta_join(R, S, lambda v, w : v["A"] == w["A"])
-assert(len(x) == 3)
-assert(
-    x
-    ==
-    [{'A': 1, 'B': 6, 'C': 7},
-     {'A': 2, 'B': 7, 'C': 8},
-     {'A': 2, 'B': 7, 'C': 9}])
+test(cross_join_1)
+test(cross_join_1)
+
+def test (f) :
+    x = f(r, s, lambda v, w : v["A"] == w["A"])
+    assert(len(x) == 3)
+    assert(
+        x
+        ==
+        [{'A': 1, 'B': 6, 'C': 7},
+         {'A': 2, 'B': 7, 'C': 8},
+         {'A': 2, 'B': 7, 'C': 9}])
+
+test(theta_join_1)
+test(theta_join_1)
 
 print("Done.")
--- examples/Select.py
+++ examples/Select.py
@@ -6,88 +6,37 @@
 
 print("Select.py")
 
-def select (r, up) :
-    return [t for t in r if up(t)]
+def select_1 (r, up) :
+    x = []
+    for v in r :
+        if up(v) :
+            x.append(v)
+    return x
 
-Student = [ \
-    {"sID" : 123, "sName" : 'Amy',    "GPA" : 3.9, "sizeHS" : 1000},
-    {"sID" : 234, "sName" : 'Bob',    "GPA" : 3.6, "sizeHS" : 1500},
-    {"sID" : 345, "sName" : 'Craig',  "GPA" : 3.5, "sizeHS" :  500},
-    {"sID" : 456, "sName" : 'Doris',  "GPA" : 3.9, "sizeHS" : 1000},
-    {"sID" : 567, "sName" : 'Edward', "GPA" : 2.9, "sizeHS" : 2000},
-    {"sID" : 678, "sName" : 'Fay',    "GPA" : 3.8, "sizeHS" :  200},
-    {"sID" : 789, "sName" : 'Gary',   "GPA" : 3.4, "sizeHS" :  800},
-    {"sID" : 987, "sName" : 'Helen',  "GPA" : 3.7, "sizeHS" :  800},
-    {"sID" : 876, "sName" : 'Irene',  "GPA" : 3.9, "sizeHS" :  400},
-    {"sID" : 765, "sName" : 'Jay',    "GPA" : 2.9, "sizeHS" : 1500},
-    {"sID" : 654, "sName" : 'Amy',    "GPA" : 3.9, "sizeHS" : 1000},
-    {"sID" : 543, "sName" : 'Craig',  "GPA" : 3.4, "sizeHS" : 2000},
-    {"sID" : 432, "sName" : 'Kevin',  "GPA" : 3.2, "sizeHS" : 1500},
-    {"sID" : 321, "sName" : 'Lori',   "GPA" : 3.2, "sizeHS" : 2500}]
-assert(len(Student) == 14)
+def select_2 (r, up) :
+    return [v for v in r if up(v)]
 
-Apply = [ \
-    {"sID" : 123, "cName" : 'Stanford', "major" : 'CS',             "decision" : True},
-    {"sID" : 123, "cName" : 'Stanford', "major" : 'EE',             "decision" : False},
-    {"sID" : 123, "cName" : 'Berkeley', "major" : 'CS',             "decision" : True},
-    {"sID" : 123, "cName" : 'Cornell',  "major" : 'EE',             "decision" : True},
-    {"sID" : 234, "cName" : 'Berkeley', "major" : 'biology',        "decision" : False},
-    {"sID" : 345, "cName" : 'MIT',      "major" : 'bioengineering', "decision" : True},
-    {"sID" : 345, "cName" : 'Cornell',  "major" : 'bioengineering', "decision" : False},
-    {"sID" : 345, "cName" : 'Cornell',  "major" : 'CS',             "decision" : True},
-    {"sID" : 345, "cName" : 'Cornell',  "major" : 'EE',             "decision" : False},
-    {"sID" : 678, "cName" : 'Stanford', "major" : 'history',        "decision" : True},
-    {"sID" : 987, "cName" : 'Stanford', "major" : 'CS',             "decision" : True},
-    {"sID" : 987, "cName" : 'Berkeley', "major" : 'CS',             "decision" : True},
-    {"sID" : 876, "cName" : 'Stanford', "major" : 'CS',             "decision" : False},
-    {"sID" : 876, "cName" : 'MIT',      "major" : 'biology',        "decision" : True},
-    {"sID" : 876, "cName" : 'MIT',      "major" : 'marine biology', "decision" : False},
-    {"sID" : 765, "cName" : 'Stanford', "major" : 'history',        "decision" : True},
-    {"sID" : 765, "cName" : 'Cornell',  "major" : 'history',        "decision" : False},
-    {"sID" : 765, "cName" : 'Cornell',  "major" : 'psychology',     "decision" : True},
-    {"sID" : 543, "cName" : 'MIT',      "major" : 'CS',             "decision" : False}]
-assert(len(Apply) == 19)
+r = [ \
+    {"A" : 1, "B" : 6},
+    {"A" : 2, "B" : 7},
+    {"A" : 3, "B" : 8}]
+assert(len(r) == 3)
 
-College = [ \
-    {"cName" : 'Stanford', "state" : 'CA', "enrollment" : 15000},
-    {"cName" : 'Berkeley', "state" : 'CA', "enrollment" : 36000},
-    {"cName" : 'MIT',      "state" : 'MA', "enrollment" : 10000},
-    {"cName" : 'Cornell',  "state" : 'NY', "enrollment" : 21000},
-    {"cName" : 'Irene',    "state" : 'TX', "enrollment" : 25000}]
-assert(len(College) == 5)
+s = [ \
+    {"A" : 4, "C" : 6},
+    {"A" : 1, "C" : 7},
+    {"A" : 2, "C" : 8},
+    {"A" : 2, "C" : 9}]
+assert(len(s) == 4)
 
-x = select(Student, lambda v : v["GPA"] > 3.7)
-print(len(x))
-print(x)
-print()
-"""
-5
-[{'sName': 'Amy',   'GPA': 3.9, 'sizeHS': 1000, 'sID': 123},
- {'sName': 'Doris', 'GPA': 3.9, 'sizeHS': 1000, 'sID': 456},
- {'sName': 'Fay',   'GPA': 3.8, 'sizeHS': 200,  'sID': 678},
- {'sName': 'Irene', 'GPA': 3.9, 'sizeHS': 400,  'sID': 876},
- {'sName': 'Amy',   'GPA': 3.9, 'sizeHS': 1000, 'sID': 654}]
-"""
+def test (f) :
+    x = f(r, lambda v : v["B"] > 6)
+    assert(len(x) == 2)
+    assert(x ==
+        [{'A': 2, 'B': 7},
+         {'A': 3, 'B': 8}])
 
-x = select(Student, lambda v : (v["GPA"] > 3.7) and (v["sizeHS"] < 1000))
-print(len(x))
-print(x)
-print()
-"""
-2
-[{'sName': 'Fay',   'GPA': 3.8, 'sizeHS': 200, 'sID': 678},
- {'sName': 'Irene', 'GPA': 3.9, 'sizeHS': 400, 'sID': 876}]
-"""
-
-x = select(Apply, lambda v : (v["cName"] == "Stanford") and (v["major"] == "CS"))
-print(len(x))
-print(x)
-print()
-"""
-3
-[{'major': 'CS', 'cName': 'Stanford', 'decision': True,  'sID': 123},
- {'major': 'CS', 'cName': 'Stanford', 'decision': True,  'sID': 987},
- {'major': 'CS', 'cName': 'Stanford', 'decision': False, 'sID': 876}]
-"""
+test(select_1)
+test(select_2)
 
 print("Done.")
